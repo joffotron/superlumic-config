ffedbb1c75d0dca7e31e32824993a1101fdecd28
Joseph Miller <joff@chargefox.com>
2023-10-29 12:25:34 +1300

    Install older non-shit version of GitKraken

---
+++ roles/profile-developer/files/gitkraken@9.7.0.rb
@@ -0,0 +1,39 @@
+cask "gitkraken@9.7.0" do
+  arch arm: "darwin-arm64", intel: "darwin"
+
+  version "9.7.0"
+  sha256 arm:   "c4eb1fcfddeabf704db6b0de1e2a5e89652f3afd55a8fc190ca400acbbc50a5e",
+         intel: "cb8a26eb7c5b241d7be7e5ecacd32cf36f010bbe88f6271507e4dfeb36e40170"
+
+  url "https://release.axocdn.com/#{arch}/GitKraken-v#{version}.zip",
+      verified: "release.axocdn.com/"
+  name "GitKraken"
+  desc "Git client focusing on productivity"
+  homepage "https://www.gitkraken.com/"
+
+  livecheck do
+    url "https://help.gitkraken.com/gitkraken-client/current/"
+    regex(/Version\s(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  app "GitKraken.app"
+
+  uninstall quit: "com.axosoft.gitkraken"
+
+  zap trash: [
+    "~/.gitkraken",
+    "~/Library/Application Support/com.axosoft.gitkraken.ShipIt",
+    "~/Library/Application Support/GitKraken",
+    "~/Library/Caches/com.axosoft.gitkraken.ShipIt",
+    "~/Library/Caches/com.axosoft.gitkraken",
+    "~/Library/Caches/GitKraken",
+    "~/Library/Cookies/com.axosoft.gitkraken.binarycookies",
+    "~/Library/HTTPStorages/com.axosoft.gitkraken",
+    "~/Library/Preferences/com.axosoft.gitkraken.helper.plist",
+    "~/Library/Preferences/com.axosoft.gitkraken.plist",
+    "~/Library/Saved Application State/com.axosoft.gitkraken.savedState",
+  ]
+end
